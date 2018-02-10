304013b85f1bb81fad7f8c968a62f681b32d0787
Adam Lancaster <lancaster28@hotmail.com>
2018-02-10 08:00:55 +0000

    add .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,23 @@
+IEx.configure([
+  colors: [enabled: true],
+  history_size: -1,
+   default_prompt: [
+     "\e[G\e[1;36m calculator\e[0m \e[1;37m 📊  >\e[0m"
+   ] |> IO.chardata_to_string,
+  inspect: [
+    limit: :infinity,
+    pretty: true,
+    syntax_colors: [
+      number: :magenta,
+      atom: :cyan,
+      string: :green,
+      boolean: :magenta,
+      nil: :magenta
+    ],
+  ],
+])
+
+# You can put aliases in here to save typing when you boot an iex shell.
+# For example you could put:
+
+import Calculator
