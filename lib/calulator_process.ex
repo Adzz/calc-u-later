f7a6ea84f6e53f0228a464c15e0f558873750cfb
Adam Lancaster <Adzz@users.noreply.github.com>
2018-03-21 16:14:29 +0000

    Update calulator_process.ex
    Add a moduledoc to calculator process

--- lib/calulator_process.ex
+++ lib/calulator_process.ex
@@ -1,6 +1,11 @@
 defmodule Calculator.Process do
+  @moduledoc """
+  This type is used to ensure that only PIDs created by a `Calculator.start` can be passed into
+  a calculator. That ensures the calculator process can only talk to ones it can do stuff with.
+  """
+ 
   @enforce_keys [:pid]
-  defstruct [:pid]
+  defstruct @enforce_keys
 
   def new(pid) do
     %__MODULE__{
